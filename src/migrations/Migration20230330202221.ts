be8ee87b918499f8960dce4371ab8ee89c4719b8
kristal <satrox120@gmail.com>
2023-04-15 19:47:43 +0200

    +UserResolver

--- src/migrations/Migration20230330202221.ts
+++ src/migrations/Migration20230330202221.ts
@@ -7,8 +7,4 @@ export class Migration20230330202221 extends Migration {
     this.addSql('alter table "user" add constraint "user_username_unique" unique ("username");');
   }
 
-  async down(): Promise<void> {
-    this.addSql('drop table if exists "user" cascade;');
-  }
-
 }
