56db0a2b564e6912a9c06626469b40bb4dc92134
Chatbot <satox120@gmail.com>
2023-05-02 15:36:21 +0200

    register resolver test

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,12 @@
-import { Resolver, Arg, InputType, Field, Mutation, Ctx } from "type-graphql";
+import {
+  Resolver,
+  Arg,
+  InputType,
+  Field,
+  Mutation,
+  Ctx,
+  ObjectType,
+} from "type-graphql";
 import { RequiredEntityData } from "@mikro-orm/core";
 import { MyContext } from "../Types";
 import { User } from "../entities/User";
@@ -12,19 +20,93 @@ export class UsernamePasswordInput {
   password: string;
 }
 
+//Error
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+//Dla erroru czy jest user czy err
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Musi mieć więcej niż 2 znaki",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Musi mieć więcej niż 3 znaki",
+          },
+        ],
+      };
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
     } as RequiredEntityData<User>);
-    await em.persistAndFlush(user);
-    return User;
+    try {
+      await em.persistAndFlush(user); 
+    } catch (err) {
+      console.log("message", err.message);
+    }
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Nie ma takiego użytkownika",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid)
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Nieprawidłowe hasło",
+          },
+        ],
+      };
+
+    return { user };
   }
 }
